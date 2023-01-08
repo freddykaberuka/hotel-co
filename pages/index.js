665a90e612295a47012de663ca743ee04353e268
freddykaberuka <freddykaberuka2009@gmail.com>
2023-01-08 23:14:02 +0200

    add homepage of hotel and configure tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export const getStaticProps= async() => {
+  
+  const res = await fetch('https://63b44f0d9f50390584ad1c20.mockapi.io/api/v1/hotels');
+  const data = await res.json();
+  return {
+    props: { hotels: data }
+  }
+}
+
+export default function Home({ hotels}) {
   return (
     <>
       <Head>
@@ -14,108 +21,24 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <h1 className="text-3xl font-bold underline"> Home Page</h1>
+        <div className="flex flex-wrap gap-6">
+        {hotels.map((hotel)=>(
+          <div className='grid max-w-sm overflow-hidden' key={hotel.id}>
+            <Link href={"/"+hotel.id}>
+            <div key={hotel.id}>
+              <Image src={hotel.avatar} width="300" height="300" className='rounded-lg' alt=''/>
+            </div>
+            <div className='pt-2'>
+              <h2 className='inline-block text-sm font-semibold text-black-700 mr-2'>{hotel.name},  {hotel.Country}</h2>
+              <p className='text-sm text-gray-700 mr-2'>{hotel.size} square meters</p>
+              <span className='text-sm text-gray-700 mr-2'>May1 - 6</span>
+              <p className='text-sm text-black-700 mr-2'>${hotel.amount} <span className='text-gray-700'>night</span></p>
+            </div>
+            </Link>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        ))}
         </div>
       </main>
     </>
