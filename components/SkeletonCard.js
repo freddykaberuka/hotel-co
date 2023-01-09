6b057d2c8087f8b21e08ac8fc31fa8ea841249a6
freddykaberuka <freddykaberuka2009@gmail.com>
2023-01-09 10:56:56 +0200

    add skeleton card

---
+++ components/SkeletonCard.js
@@ -0,0 +1,17 @@
+export const SkeletonCard = () => {
+    return ( 
+        <div className='grid max-w-sm overflow-hidden animate-pulse'>
+            <div className="bg-gray-400 rounded-lg">
+              <div className="rounded-lg h-60 w-72"/>
+            </div>
+            <div className='pt-2'>
+              <h2 className='inline-block text-sm font-semibold text-black-700 mr-2 bg-gray-400 h-6 w-9/12'></h2>
+              <p className='text-sm text-gray-700 mr-2 bg-gray-400 h-6 w-9/12'></p>
+              <span className='text-sm text-gray-700 mr-2 bg-gray-400 h-6 w-9/12'></span>
+              <p className='text-sm text-black-700 mr-2 bg-gray-400 h-6 w-9/12'></p>
+            </div>
+          </div>
+     );
+}
+ 
+export default SkeletonCard;
