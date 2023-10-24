b41771504b762ff405bf878cc70e4b753a24ee44
freddykaberuka <freddykaberuka2009@gmail.com>
2023-10-24 13:34:59 +0200

    add react-query

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { QueryClientProvider } from '../utils/queryClients';
+import { queryClient } from '../utils/queryClients';
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
